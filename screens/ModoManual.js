102489dc64b3338974a823e2f072ed0b1d57b5ed
gabiteixeira_09 <gabiteixeir@gmail.com>
2024-06-25 14:33:10 -0300

    mudança modo manual

--- screens/ModoManual.js
+++ screens/ModoManual.js
@@ -5,28 +5,25 @@ const { height } = Dimensions.get('window');
 
 export default function ModoManualScreen() {
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Modo Manual</Text>
-        <Text style={styles.description}>Como funciona a lógica: </Text>
-        <Image source={require('../assets/CodigoManual.png')} style={styles.image} resizeMode="contain" />
+     <View style={styles.container}>
+        <Image source={require('../assets/Modo Manual.png')} style={styles.image} resizeMode="contain" />
       </View>
-    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingVertical: 20,
-    backgroundColor: '#FFE4E1',
-  },
+  //scrollContainer: {
+    //flexGrow: 1,
+    //justifyContent: 'flex-start',
+    //alignItems: 'center',
+    //paddingVertical: 20,
+    //backgroundColor: '#FFE4E1',
+  //},
   container: {
     flex: 1,
     alignItems: 'center',
     paddingHorizontal: 20,
+    backgroundColor: '#FFE4E1',
   },
   title: {
     fontSize: 24,
@@ -35,9 +32,10 @@ const styles = StyleSheet.create({
   },
   image: {
     width: '100%',
-    height: undefined,
+    height: 750,
     aspectRatio: 1, // Mantém a proporção da imagem
     marginBottom: 20,
+    marginTop:0,
     resizeMode: 'contain', // Garante que a imagem seja contida dentro das dimensões
   },
   description: {
