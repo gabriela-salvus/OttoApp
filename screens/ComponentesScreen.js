f1dbb12420be59dfa1fa93f68975d3d0c426622c
gabiteixeira_09 <gabiteixeir@gmail.com>
2024-06-26 13:11:08 -0300

    modificaçao imagens componentes

--- screens/ComponentesScreen.js
+++ screens/ComponentesScreen.js
@@ -4,27 +4,27 @@ import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 export default function ComponentesScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Componentes eletrônicos</Text>
+      <Image source={require('../assets/images/componenteselet (1).png')} style={styles.imageTopo} />
       <View style={styles.componentContainer}>
-        <Image source={require('../assets/images/servomotor.jpeg')} style={styles.image} />
+        <Image source={require('../assets/images/f1ca6741-1fbb-48fa-b321-8e7d313560b4.png')} style={styles.image} />
         <Text style={styles.description}>
           <Text style={styles.boldText}>Servo motor:</Text> É um atuador que permite controle preciso de posição, velocidade e aceleração. Ele é composto por um motor, um sensor de posição e um circuito de controle.
         </Text>
       </View>
       <View style={styles.componentContainer}>
-        <Image source={require('../assets/images/modulo.jpeg')} style={styles.image} />
+        <Image source={require('../assets/images/590cd2c8-d502-4466-ac84-b82de7891eab.png')} style={styles.image} />
         <Text style={styles.description}>
           <Text style={styles.boldText}>Modulo Hc05:</Text> Dispositivo que permite comunicação sem fio entre dispositivos eletrônicos via Bluetooth.
         </Text>
       </View>
       <View style={styles.componentContainer}>
-        <Image source={require('../assets/images/sensor.jpeg')} style={styles.image} />
+        <Image source={require('../assets/images/99fde358-0add-4cdf-957e-d6280eeff395.png')} style={styles.image} />
         <Text style={styles.description}>
           <Text style={styles.boldText}>Sensor ultrassônico:</Text> Dispositivo que utiliza ondas sonoras de alta frequência para medir distâncias ou detectar objetos.
         </Text>
       </View>
       <View style={styles.componentContainer}>
-        <Image source={require('../assets/images/arduino.jpeg')} style={styles.image} />
+        <Image source={require('../assets/images/f6af149c-d7aa-486e-92c4-04fbbd9cd2a2.png')} style={styles.image} />
         <Text style={styles.description}>
           <Text style={styles.boldText}>Arduino nano:</Text> É uma pequena e completa placa microcontroladora, que serve como o cérebro do robo, ela que irá ser responsavel pelas suas acões.
         </Text>
@@ -36,9 +36,9 @@ export default function ComponentesScreen() {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    padding: 20,
+    padding: 0,
     alignItems: 'center',
-    backgroundColor: '#B0E0E6',
+    backgroundColor: '#F0FFFF',
   },
   title: {
     fontSize: 30,
@@ -53,7 +53,15 @@ const styles = StyleSheet.create({
   },
   componentContainer: {
     marginBottom: 20,
+    marginHorizontal:20,
     alignItems: 'center',
+  },
+  imageTopo:{
+    width: '100%',
+    height: 250,
+    paddingTop: 0,
+    paddingHorizontal:0,
+    marginTop: 0,
   },
   image: {
     width: 200,
@@ -72,9 +80,10 @@ const styles = StyleSheet.create({
   },
   boldText: {
     fontWeight: 'bold',
-    color: 'white',
-    textShadowColor: 'black',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 2.5,
+    color: '#4682B4',
+    fontSize: 18,
+    //textShadowColor: 'black',
+    //textShadowOffset: { width: 0, height: 0 },
+    //textShadowRadius: 2.5,
   },
 });
